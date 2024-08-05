f3e643ac0cda87130c9157fccac271caa4156bb5
Vijay Prince <vjprince@amazon.com>
2024-08-05 13:09:34 -0600

    Logic to update from parent branch

--- .codecatalyst/git-merge.sh
+++ .codecatalyst/git-merge.sh
@@ -1,24 +1,70 @@
 #!/bin/sh
 
-USER_NAME="Vijay Prince"
-EMAIL="vjprince@amazon.com"
+echo "User Name: $1"
+echo "User Email: $2"
+echo "User ID: $3"
+echo "PAT: $4"
+if [ $# -ne 4 ]; then
+  echo "Expected 4 arguments 'User Name', 'User Email', 'User Id', 'token'"
+  exit 1   
+fi
 
-git config --list
-git remote add upstream https://github.com/vprince1/aws-ai-intelligent-document-processing.git
-git fetch upstream
-git -c "user.name=Vijay Prince" -c "user.email=vjprince@amazon.com" merge upstream/main --allow-unrelated-histories
+USER_NAME=$1
+USER_EMAIL=$2
+CLEAN_DIR="clean"
+USER_ID=$3
+PAT=$4
 
-echo "git -c "user.name=${USER_NAME}" -c "user.email=${EMAIL}" merge upstream/main --allow-unrelated-histories"
-result=$(git -c "user.name=${USER_NAME}" -c "user.email=${EMAIL}" merge upstream/main --allow-unrelated-histories)
-if [ $? -eq 0 ]
-then
-  echo "Merge Success"
-  echo "$result"
-  #git push
-  exit 0
+pwd
+cd ..
+pwd
+sudo yum -y install rsync
+if [ -d "$CLEAN_DIR" ]; then
+  echo "Clean existing directory"
+  cd clean
+  rm -rf aws-ai-intelligent-document-processing
 else
-  echo "Merge Failed"
-  git diff
-  exit 1
+  echo "Create new directory"
+  mkdir clean
+  cd clean
+fi
+echo "git clone https://github.com/vprince1/aws-ai-intelligent-document-processing.git"
+git clone https://github.com/vprince1/aws-ai-intelligent-document-processing.git
+echo "Folder of clean parent repo: $(pwd)"
+cd ./../aws-ai-intelligent-document-processing
+echo "Folder of cloned repo: $(pwd)"
+echo "rsync -aq --exclude .codecatalyst --exclude .git ./../clean/aws-ai-intelligent-document-processing/* ."
+rsync -aq --exclude .codecatalyst --exclude .git ./../clean/aws-ai-intelligent-document-processing/* .
+first_commit_time=$(date --date "$(git show -s --format=%cI $(git rev-list --max-parents=0 HEAD))" +"%s")
+echo "First Commit Time: $first_commit_time"
+current_time=$(date --date "$(date)" +"%s")
+echo "Current Time: $current_time"
+time_diff=$((current_time - $first_commit_time))
+echo "Time Difference: $time_diff"
+if [ $time_diff > 300 ]; then
+  git diff-index --quiet HEAD --
+  if [ $? -eq 1 ]; then
+    echo "Git remotes"
+    git remote -v
+    git config --global user.email "$USER_EMAIL"
+    git config --global user.name "$USER_NAME"
+    git add .
+    git commit -m "Updated from parent https://github.com/vprince1/aws-ai-intelligent-document-processing.git"
+    git branch temp-branch
+    git checkout main
+    git merge temp-branch
+    echo "Before Push"
+    remote_url=$(git config --get remote.origin.url)
+    echo "Remote URL: $remote_url"
+    repo_without_protocol="${remote_url#https://}"
+    echo "Remote URL without protocol: $repo_without_protocol"
+    echo "git push --repo https://$USER_ID:$PAT@$repo_without_protocol"
+    git push --repo https://$USER_ID:$PAT@$repo_without_protocol
+    echo "After push"
+  else
+    echo "No code changes"
+  fi
+else
+  echo "Ignore merge in new repo - Assuming the first commit time of the new repo will be less than 5 minutes."
 fi
 
